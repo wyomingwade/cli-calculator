dc4b0d4d7aa826455711ea77318584d5693293d2
Wyoming Wade <wyo6369@gmail.com>
2024-11-13 02:50:34 -0500

    basic calculator done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use text_io::read;
+
+const VERBOSE: bool = false;
+
 fn main() {
-    println!("Hello, world!");
+    // get user input
+    println!("Enter the first number: ");
+    let a: f64 = read!();
+    println!("Enter the operator (+, -, *, /): ");
+    let operator: char = read!();
+    println!("Enter the second number: ");
+    let b: f64 = read!();
+
+    // calculate the result
+    let result: f64 = calculate(a, operator, b);
+    println!("Answer:");
+    // write out the whole expression if verbose is true, otherwise just the answer
+    if VERBOSE {
+        println!("{} {} {} = {}", a, operator, b, result);
+    }
+    else {
+        println!("{}", result);
+    }
+    
+}
+
+fn calculate(a: f64, operator: char, b: f64) -> f64 {
+    let mut result = 0.0;
+
+    match operator {
+        '+' | 'p' => result = a + b,
+        '-' | 'm' => result = a - b,
+        '*' | 't' => result = a * b,
+        '/' | 'd' => result = a / b,
+        _ => println!("Invalid operator given. Returning 0.0"),
+    }
+
+    return result;
 }
